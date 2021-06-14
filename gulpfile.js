0252b3a2be96a1304f1eb2d0d66fb919b080194f
polinav2020 <polinapiksi2019@mail.ru>
2021-06-14 06:39:37 +0300

    init: create html for home page

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,9 @@ function html() {
 function scss() {
   return src("src/scss/**.scss")
   .pipe(sass())
-  .pipe(autoprefixer())
+  .pipe(autoprefixer({
+    cascade: false
+  }))
   .pipe(csso())
   .pipe(concat("index.css"))
   .pipe(dest("dist"))
@@ -44,11 +46,12 @@ function serve() {
   sync.init({
     server: "./dist"
   })
-  watch("src/**html", series(html)).on("change", sync.reload)
-  watch("src/scss/**scss", series(scss)).on("change", sync.reload)
+
+  watch("src/**.html", series(html)).on("change", sync.reload)
+  watch("src/img/", series(html, scss)).on("change", sync.reload)
+  watch("src/scss/**.scss", series(scss)).on("change", sync.reload)
 }
 
-
+exports.clear = clear
 exports.build = series(clear, scss, html)
 exports.serve = series(clear, scss, html, serve)
-exports.clear = clear
